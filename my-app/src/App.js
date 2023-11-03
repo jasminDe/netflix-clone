bbf0f620f187eb0164a22191833c948ceb678c3b
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-11-03 01:45:49 +0000

    added row component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,14 @@
-import { React } from 'react';
+import React from 'react';
 import './App.css';
+import Row from './Row';
 
 function App(){
+<div className='App'>
+   <h1>Hey There</h1>
+   <Row title="NETFLIX ORIGINALS" /> 
+   <Row title="Trending Now" /> 
 
+</div>
 }
 
 export default App
